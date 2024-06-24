7a20064c6ba8ee390b8af4d7823dac6b6dde4d31
Chionesu <sapienti.Dei@outlook.com>
2024-06-23 23:16:04 -0400

    Directory tree clean up

--- .SANDBOX.R
+++
@@ -1,2 +0,0 @@
-identifier <- smart.data::as.taxonomy(list(term = "identifier", desc = "Identifies unique instances in a set"))
-jsonlite::toJSON(as.list(identifier) |> purrr::modify_at("law", rlang::expr_deparse)) |> smart.data::as.taxonomy()
